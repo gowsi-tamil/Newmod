99b42c17aa151f931e3da725c6766028cd176b97
mouly <mouly@LAPTOP-OPSUCUV0>
2020-12-29 09:54:53 +0530

    commited

---
+++ gowsi/src/gowsi/Anot.java
@@ -0,0 +1,15 @@
+package gowsi;
+
+public class Anot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s,f,ss;
+		s=94;
+		f=43;
+		ss=f+s;
+		System.out.print(ss);
+
+	}
+
+}
